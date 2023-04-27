9051516c9d2d27249c29f3092b59856205b26ad6
Максим Луговской <lugovskou.myu@yandex.ru>
2023-04-27 23:22:49 +0500

    [add]: Добавил пользовательский роутер

---
+++ routes/routersUser.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+// const {
+//   validateUserInfo,
+//   // validateUserId
+// } = require('../middlewares/validation');
+
+// const {
+//   // findUserbyId,
+//   updateUser,
+//   getUserInfo,
+// } = require('../controllers/users');
+
+// router.get('/users/me', getUserInfo);
+// router.get('/users/:id', findUserbyId);
+// router.patch('/users/me', updateUser);
+
+module.exports = router;
