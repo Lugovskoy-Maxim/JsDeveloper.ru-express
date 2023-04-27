67c7cffa3d485013bc4f8da1a441b82c8dc80d24
Максим Луговской <lugovskou.myu@yandex.ru>
2023-04-27 23:20:31 +0500

    [update]: Добавил кастомные ошибки

--- errors/index.js
+++ errors/index.js
@@ -1,12 +1,16 @@
 // const AppError = require('./AppError.js');
+const AuthError = require('./AuthError');
 // const HTTPError = require('./HTTPError.js');
 // const ServerError = require('./ServerError.js');
-// const ConflictError = require('./ConflictError.js');
+const ConflictError = require('./ConflictError.js');
 const NotFoundError = require('./NotFoundError');
 // const ForbiddenError = require('./ForbiddenError.js');
-// const BadRequestError = require('./BadRequestError.js');
+const BadRequestError = require('./BadRequestError.js');
 // const UnauthorizedError = require('./UnauthorizedError.js');
 
 module.exports = {
+  AuthError,
   NotFoundError,
+  ConflictError,
+  BadRequestError,
 }
